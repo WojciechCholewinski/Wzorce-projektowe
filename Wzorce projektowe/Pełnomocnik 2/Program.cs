101477237596eedb442eae3c4a2195ef5566f653
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2022-11-27 03:34:21 +0100

    Początek zadania

--- Wzorce projektowe/Pełnomocnik 2/Program.cs
+++ Wzorce projektowe/Pełnomocnik 2/Program.cs
@@ -1,2 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pelnomocnik
+{
+
+    public class User
+    {
+
+        private bool HasAdminPrivilages;
+
+        // konstruktor? jak jest tworzony obiekt?
+
+        public void MakeAdmin()
+        {
+            // co robi?
+        }
+
+        public bool IsAdmin()
+        {
+            // co zwraca?
+        }
+
+    }
+
+    public interface Information
+    {
+        public abstract void DisplayData();
+        public abstract void DisplayRestrictedData();
+    }
+
+    public class Database : Information
+    {
+
+        private Dictionary<string, double> Map;
+
+        public Database()
+        {
+            // stworzenie bazy użytkowników
+            // i uzupełnienie wartości
+        }
+
+        // wyświetlenie listy użytkowników
+
+        // wyświetlenie ujawniające zarobki
+
+    }
+
+    public class DatabaseGuard : Information
+    {
+
+        private Database DB;
+        private User user;
+
+        public DatabaseGuard(User u)
+        {
+            // stworzenie obiektu DB i przypisanie do pola
+            // u? pewnie pole ;)
+        }
+
+        public void DisplayData()
+        {
+            DB.DisplayData();
+        }
+
+        public void DisplayRestrictedData()
+        {
+            // sprawdzenie uprawnień i odpowienie działanie
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Zbyszek = new User(false);
+            var db = new DatabaseGuard(Zbyszek);
+
+            db.DisplayData();
+
+            Console.WriteLine("---------------------------------------------------------");
+
+            db.DisplayRestrictedData();
+
+            Console.WriteLine("---------------------------------------------------------");
+
+            Zbyszek.MakeAdmin();
+            db.DisplayRestrictedData();
+
+        }
+    }
+
+}
