7545030fc549823b989b1ce02b6ecc8e019e2461
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2023-01-07 22:10:51 +0100

    create new "Metoda_szablonowa" project

---
+++ Wzorce projektowe/Metoda szablonowa/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Metoda_szablonowa
+{
+    abstract class ZamowienieTemplatka
+    {
+
+        //
+        //
+        //
+
+        public void dodanieGratisu()
+        {
+            Console.WriteLine("Dodano gratis...");
+        }
+
+        public void przetwarzajZamowienie(bool czyGratis)
+        {
+            //
+            //
+            //
+        }
+    }
+
+
+    class ZamowienieOnline : ZamowienieTemplatka
+    {
+
+        override public void doKoszyk()
+        {
+            Console.WriteLine("Kompletowanie zamówienia...");
+            Console.WriteLine("Ustawiono parametry wysyłki...");
+        }
+
+        override public void doPlatnosc()
+        {
+            Console.WriteLine("Płatność...");
+        }
+
+        override public void doDostawa()
+        {
+            Console.WriteLine("Wysyłka...");
+        }
+
+    }
+
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //
+            //
+            //
+
+            ZamowienieTemplatka zamowienieStacjonarne = new ZamowienieStacjonarne();
+            zamowienieStacjonarne.przetwarzajZamowienie(false);
+        }
+    }
+}
