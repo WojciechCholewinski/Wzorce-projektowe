2048ea23200993c5de5ec3dc90cef9fdf75d0d39
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2023-01-08 13:47:44 +0100

    Final program polecenie

--- Wzorce projektowe/Polecenie/Program.cs
+++ Wzorce projektowe/Polecenie/Program.cs
@@ -1,9 +1,8 @@
-﻿
-using System;
+﻿using System;
 
 public interface IPolecenie
 {
-    /* UZUPEŁNIĆ */
+    void wykonaj();
 }
 
 public class KomendaWlacz : IPolecenie
@@ -11,7 +10,7 @@ public class KomendaWlacz : IPolecenie
     Lampa lampa;
     public KomendaWlacz(Lampa lampa)
     {
-        /* UZUPEŁNIĆ */
+        this.lampa = lampa;
     }
     public void wykonaj()
     {
@@ -21,15 +20,30 @@ public class KomendaWlacz : IPolecenie
 
 public class KomendaWylacz : IPolecenie
 {
-    /* UZUPEŁNIĆ */
+    Lampa lampa;
+    public KomendaWylacz(Lampa lampa)
+    {
+        this.lampa = lampa;
+    }
+    public void wykonaj()
+    {
+        lampa.wylacz();
+    }
 }
-
 
 public class Lampa
 {
-    /* UZUPEŁNIĆ */
+    private bool stan;
 
-    /* UZUPEŁNIĆ */
+    public void wlacz()
+    {
+        stan = true;
+    }
+
+    public void wylacz()
+    {
+        stan = false;
+    }
 
     public bool sprawdz()
     {
@@ -37,34 +51,38 @@ public class Lampa
     }
 }
 
-
 public class Pilot
 {
     private IPolecenie polecenie;
 
-    /* UZUPEŁNIĆ */
+    public void ustawPolecenie(IPolecenie polecenie)
+    {
+        this.polecenie = polecenie;
+    }
 
+    public void wcisnijGuzik()
+    {
+        polecenie.wykonaj();
+    }
 }
-
 
 class Program
 {
     static void Main(string[] args)
     {
-        /* UZUPEŁNIĆ */
-
+        Lampa lampa = new Lampa();
         IPolecenie wlacz = new KomendaWlacz(lampa);
-        /* UZUPEŁNIĆ */
+        IPolecenie wylacz = new KomendaWylacz(lampa);
+        Pilot pilot = new Pilot();
 
         Console.WriteLine(lampa.sprawdz() ? "Lampa włączona" : "Lampa wyłączona");
 
         pilot.ustawPolecenie(wlacz);
         pilot.wcisnijGuzik();
-        /* UZUPEŁNIĆ */
+        Console.WriteLine(lampa.sprawdz() ? "Lampa włączona" : "Lampa wyłączona");
 
         pilot.ustawPolecenie(wylacz);
         pilot.wcisnijGuzik();
-        /* UZUPEŁNIĆ */
-
+        Console.WriteLine(lampa.sprawdz() ? "Lampa włączona" : "Lampa wyłączona");
     }
 }
