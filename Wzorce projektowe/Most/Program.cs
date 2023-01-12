0de27969397048fe9187bc660b77b1b8cb97fc6a
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2023-01-12 21:23:33 +0100

    Final program Most

--- Wzorce projektowe/Most/Program.cs
+++ Wzorce projektowe/Most/Program.cs
@@ -1,131 +1,177 @@
 ﻿using System;
 
-namespace Most
+
+public interface ITelewizor
 {
-	public interface ITelewizor
-	{
-
-		int Kanal { get; set; }
-		//
-		//
-		void ZmienKanal(int kanal);
-
-	}
+    string Name { get; set; }
+    int Kanal { get; set; }
+    void Wlacz();
+    void Wylacz();
+    void ZmienKanal(int kanal);
+}
 
 
 
-	public class TvLg : ITelewizor
-	{
+public class TvLg : ITelewizor
+{
+    public string Name { get; set; } = "LG";
 
-		public TvLg()
-		{
-			this.Kanal = 1;
-		}
+    public TvLg()
+    {
+        this.Kanal = 1;
+    }
 
-		public int Kanal { get; set; }
+    public int Kanal { get; set; }
 
-		public void Wlacz()
-		{
-			Console.WriteLine("Telewizor LG - włączam się.");
-		}
+    public void Wlacz()
+    {
+        Console.WriteLine("Telewizor LG - włączam się.");
+    }
 
-		public void Wylacz()
-		{
-			//
-			//
-		}
+    public void Wylacz()
+    {
+        Console.WriteLine("Telewizor LG - wyłączam się.");
+        //
+    }
 
-		public void ZmienKanal(int kanal)
-		{
-			//
-			//
-		}
+    public void ZmienKanal(int kanal)
+    {
+        Kanal = kanal;
+        Console.WriteLine("Telewizor LG - zmianiam kanał: " + Kanal);
 
-	}
+    }
 
+}
+public class TvXiaomi : ITelewizor
+{
+    public string Name { get; set; } = "Xiaomi";
+    public TvXiaomi()
+    {
+        this.Kanal = 1;
+    }
 
+    public int Kanal { get; set; }
 
-	public class TvXiaomi : ITelewizor
-	{
+    public void Wlacz()
+    {
+        Console.WriteLine("Telewizor Xiaomi - włączam się.");
+    }
 
-		//
-		//
+    public void Wylacz()
+    {
+        Console.WriteLine("Telewizor Xiaomi - wyłączam się.");
+        //
+    }
 
-	}
+    public void ZmienKanal(int kanal)
+    {
+        Kanal = kanal;
+        Console.WriteLine("Telewizor Xiaomi - zmianiam kanał: " + Kanal);
 
+    }
 
-
-	public abstract class PilotAbstrakcyjny
-	{
-
-		private ITelewizor tv;
-
-		public PilotAbstrakcyjny(ITelewizor tv)
-		{
-			//
-			//
-		}
-
-		//
-		//
-
-		public void ZmienKanal(int kanal)
-		{
-			//
-			//
-		}
-
-	}
+}
 
 
 
-	public class PilotHarmony : PilotAbstrakcyjny
-	{
+public abstract class PilotAbstrakcyjny
+{
 
-		public PilotHarmony(ITelewizor tv) : base(tv) { }
+    private ITelewizor tv;
 
-		public void DoWlacz()
-		{
-			Console.WriteLine("Pilot Harmony - włącz telewizor...");
-			Wlacz();
-		}
+    public PilotAbstrakcyjny(ITelewizor tv)
+    {
+        this.tv = tv;
+    }
 
-		//
-		//
+    public void Wlacz()
+    {
+        tv.Wlacz();
+    }
 
-	}
-
-	public class PilotLG : PilotAbstrakcyjny
-	{
-
-		//
-		//
-
-		public void DoWylacz()
-		{
-			Console.WriteLine("Pilot LG - wyłącz telewizor...");
-			Wylacz();
-		}
-
-	}
+    public void Wylacz()
+    {
+        tv.Wylacz();
+    }
 
 
+    public void ZmienKanal(int kanal)
+    {
+        tv.Kanal = kanal;
+        Console.WriteLine("Telewizor " + tv.Name + " - zmieniam kanał: " + kanal);
+    }
 
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
+}
 
-			ITelewizor tv = new TvLg();
-			//
-			//
 
-			pilotHarmony.DoWlacz();
-			Console.WriteLine("Kanał: " + tv.Kanal);
-			pilotLG.DoZmienKanal(100);
-			Console.WriteLine("Kanał: " + tv.Kanal);
-			pilotHarmony.DoWylacz();
 
-		}
-	}
+public class PilotHarmony : PilotAbstrakcyjny
+{
+
+    public PilotHarmony(ITelewizor tv) : base(tv) { }
+
+    public void DoWlacz()
+    {
+        Console.WriteLine("Pilot Harmony - włącz telewizor...");
+        Wlacz();
+    }
+    public void DoWylacz()
+    {
+        Console.WriteLine("Pilot Harmony - wyłącz telewizor...");
+        Wylacz();
+    }
+    public void DoZmienKanal(int kanal)
+    {
+        Console.WriteLine("Pilot Harmony - zmienia kanał...");
+        ZmienKanal(kanal);
+    }
+
+
+}
+
+public class PilotLG : PilotAbstrakcyjny
+{
+
+    public PilotLG(ITelewizor tv) : base(tv) { }
+
+    public void DoWlacz()
+    {
+        Console.WriteLine("Pilot LG - włącz telewizor...");
+        Wlacz();
+    }
+    public void DoWylacz()
+    {
+        Console.WriteLine("Pilot LG - wyłącz telewizor...");
+        Wylacz();
+
+    }
+    public void DoZmienKanal(int kanal)
+    {
+        Console.WriteLine("Pilot LG - zmienia kanał...");
+        ZmienKanal(kanal);
+
+    }
+
+
+}
+
+
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        ITelewizor tv = new TvLg();
+
+        PilotHarmony pilotHarmony = new PilotHarmony(tv);
+        pilotHarmony.DoWlacz();
+        Console.WriteLine("Sprawdź kanał - bieżący kanał: " + tv.Kanal);
+
+        PilotLG pilotLG = new PilotLG(tv);
+        pilotLG.DoZmienKanal(100);
+        Console.WriteLine("Sprawdź kanał - bieżący kanał: " + tv.Kanal);
+        pilotHarmony.DoWylacz();
+
+    }
 }
